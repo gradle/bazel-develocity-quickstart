72f46d9d9516f044dc9d3525522532476aef98b3
Brian Saghy <bsaghy@gradle.com>
2023-12-05 12:44:24 -0400

    Define the bazelrc filename as a variable.

--- provision_bazelrc.sh
+++ provision_bazelrc.sh
@@ -2,9 +2,10 @@
 # See README.md for usage
 SERVER=$1
 KEY=$2
+BAZELRC_FILE="user.bazelrc"
 
-echo "common:develocity --remote_cache=grpcs://${SERVER}" > user.bazelrc
-echo "common:develocity --bes_backend=grpcs://${SERVER}" >> user.bazelrc
-echo "common:develocity --bes_results_url=https://${SERVER}/build" >> user.bazelrc
-echo "common:develocity --remote_header=Authorization=${KEY}" >> user.bazelrc
-echo "common:develocity --bes_header=Authorization=${KEY}" >> user.bazelrc
+echo "common:develocity --remote_cache=grpcs://${SERVER}" > $BAZELRC_FILE
+echo "common:develocity --bes_backend=grpcs://${SERVER}" >> $BAZELRC_FILE 
+echo "common:develocity --bes_results_url=https://${SERVER}/build" >> $BAZELRC_FILE
+echo "common:develocity --remote_header=Authorization=${KEY}" >> $BAZELRC_FILE
+echo "common:develocity --bes_header=Authorization=${KEY}" >> $BAZELRC_FILE
