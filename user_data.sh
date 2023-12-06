a3cfd3a5e89c3b73322d96330e01ea2a8bbbc6e3
Brian Saghy <bsaghy@gradle.com>
2023-12-05 22:26:04 -0400

    Add the git branch and clean/dirty status to the build scan.

--- user_data.sh
+++ user_data.sh
@@ -6,6 +6,13 @@
 # Creates a Tag called "DEMO"
 echo SCAN_TAG_DEMO ""
 
+# Tag as "Dirty" if there are modifications
+if [ -z "$(git status --porcelain)" ]; then
+  echo SCAN_TAG_CLEAN ""
+else
+  echo SCAN_TAG_DIRTY ""
+fi
+
 # Creates Links in the Build Scan
 echo SCAN_LINK_Bazel_Docs https://bazel.build/docs
 echo SCAN_LINK_Bazel_Reference https://bazel.build/reference
@@ -14,3 +21,4 @@ echo SCAN_LINK_Develocity https://gradle.com/develocity/
 # Creates a Scan Key/Value
 echo SCAN_VALUE_Day_of_the_week $(date +%w)
 echo SCAN_VALUE_GIT_COMMIT_ID $(git rev-parse HEAD)
+echo SCAN_VALUE_GIT_BRANCH $(git branch --show-current)
